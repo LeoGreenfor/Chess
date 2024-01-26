a1e615e3e3b89036aa5fe74bdccb557ed1939dde
LeoGreenfor <97034848+LeoGreenfor@users.noreply.github.com>
2024-01-27 00:58:31 +0200

    upd

---
+++ Chess/Assets/Scripts/System/MenuController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class MenuController : MonoBehaviour
+{
+    [SerializeField] private Outline _objOutline;
+    [SerializeField] private Canvas _eventCanvas;
+
+    public void OnMouseExit()
+    {
+        _objOutline.OutlineWidth = 0;
+        Debug.LogWarning("exit obj");
+    }
+
+    public void OnMouseEnter()
+    {
+        Debug.LogWarning("enter obj");
+        _objOutline.OutlineWidth = 5;
+    }
+
+}
