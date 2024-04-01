dcb213b39083cd2feb1db48f24d2512154433947
LeoGreenfor <97034848+LeoGreenfor@users.noreply.github.com>
2024-04-02 00:38:23 +0300

    Update MapController.cs

--- Chess/Assets/Scripts/System/MapController.cs
+++ Chess/Assets/Scripts/System/MapController.cs
@@ -10,27 +10,9 @@ public class MapController : MonoBehaviour
     [SerializeField] private SceneAsset[] scenes;
     [SerializeField] private Button[] mapLevelButton;
 
-    private void Start()
-    {
-        /*for (int i = 0; i < mapLevelButton.Length; i++)
-        {
-            Debug.LogError(i);
-            mapLevelButton[i].onClick.AddListener(() => LoadLevel(scenes[i]));
-        }*/
-    }
-
     public void LoadLevel(SceneAsset scene)
     {
         Debug.LogError("a");
         SceneManager.LoadScene(scene.name);
-    }
-
-    private void OnDestroy()
-    {
-        /*for (int i = 0; i < mapLevelButton.Length; i++)
-        {
-            Debug.LogError(i);
-            mapLevelButton[i].onClick.RemoveListener(() => LoadLevel(scenes[i]));
-        }*/
     }
 }
